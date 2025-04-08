6ce2122eae1baae54813f0139bda3e2e96509235
sergio-ferreira-ruiz <seferu471@gmail.com>
2025-04-09 00:09:55 +0200

    Add files via upload

---
+++ fabfaz.py
@@ -0,0 +1,18 @@
+import os, sys
+from fab import tanper
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def pintafab():
+    estadif=open(sys.argv[1],'r')
+    cuartos,teams= tanper(estadif)
+    salida=''
+    for cuarto, parcial in cuartos.items():
+        salida+='{}º Cuarto.&emsp;'.format(cuarto)
+        for equipo, puntos in parcial.items():            
+            salida+='{}:&ensp;{}&emsp;'.format(teams[equipo],puntos)
+        salida+='<hr>'
+    estadif.close()
+    return salida
+if __name__=="__main__":
+    app.run()
